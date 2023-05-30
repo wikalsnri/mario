66429995f5268bb8030b57664ffd1b23faf2f18c
wikalsnri <wikalsnri@gmail.com>
2023-05-30 15:53:46 +0700

    mario jumping on the pipe

--- js/script.js
+++ js/script.js
@@ -12,8 +12,9 @@ const jump = () => {
 
 const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','')
 
-    if (pipePosition <= 120) {
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80      ) {
         pipe.style.animation = "none"
         pipe.style.left = `${pipePosition}px`
     }
