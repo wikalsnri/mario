9c6f65af6f81f1f01e02d464a1edb06ce8068faf
wikalsnri <wikalsnri@gmail.com>
2023-07-02 21:34:35 +0700

    add clouds img

--- js/script.js
+++ js/script.js
@@ -17,8 +17,15 @@ const loop = setInterval(() => {
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80      ) {
         pipe.style.animation = "none"
         pipe.style.left = `${pipePosition}px`
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`
+        mario.src = './images/Gambar/game-over.png';
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
     }
 }, 10);
 
 document.addEventListener('keydown', jump)
+
  
